e277c1d973bcd4848905d91fa29a84b2caf71d8c
Krishna Sivapothanathan <krishnasivapothanathan@gmail.com>
2024-01-18 23:02:12 +0530

    dark mode toggle initialized

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var settingsmenu = document.querySelector(".settings-menu");
 
-function settingsMenuToggle(){
+function settingsMenuToggle() {
     settingsmenu.classList.toggle("settings-menu-height");
 }
